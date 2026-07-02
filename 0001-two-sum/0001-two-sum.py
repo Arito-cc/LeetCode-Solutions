733deda887e3133f04da274289dc4ebefa744677
Abhishek Chaudhary (Arito) <147802600+Arito-cc@users.noreply.github.com>
2026-07-02 12:39:09 +0530

    Time: 0 ms (100%), Space: 20.7 MB (7.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen={}
 
         for i in range(0,len(nums)):
@@ -16,5 +9,3 @@ class Solution(object):
                 seen[nums[i]]=i
 
         return []
-
-        
