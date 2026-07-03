e4b66d2e3d1ba35f4fea073314bb8d622c274f35
Abhishek Chaudhary (Arito) <147802600+Arito-cc@users.noreply.github.com>
2026-07-03 11:30:20 +0530

    Time: 3 ms (60.74%), Space: 44.6 MB (51.89%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+
+        int l = 0;
+        int r = s.length() - 1;
+        while (l < r) {
+            if (Character.isLetterOrDigit(s.charAt(l))) {
+                if (Character.isLetterOrDigit(s.charAt(r))) {
+                    if (s.charAt(l) != s.charAt(r)) {
+                        return false;
+                    }
+                    l++;
+                    r--;
+                } else {
+                    r--;
+                }
+            } else {
+                l++;
+            }
+
+        }
+        return true;
+    }
+}
