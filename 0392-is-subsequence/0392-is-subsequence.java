c4b36d339f0825c17468303a7f86e8493afbadef
Abhishek Chaudhary (Arito) <147802600+Arito-cc@users.noreply.github.com>
2026-07-06 13:04:22 +0530

    Time: 2 ms (72.15%), Space: 42.6 MB (79.73%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) {
+            return true;
+        }
+        if (t.length() == 0 && s.length() != 0) {
+            return false;
+        }
+        int count = 0;
+        int sp = 0;
+        for (int i = 0; i < t.length(); i++) {
+            if (count < s.length()) {
+                if (s.charAt(sp) == t.charAt(i)) {
+                    count++;
+                    sp++;
+                }
+            }
+        }
+        if (s.length() - count == 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
