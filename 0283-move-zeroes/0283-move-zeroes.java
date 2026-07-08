218dd2e804efbacae389c2353cbce0b0df4bf1c1
Abhishek Chaudhary (Arito) <147802600+Arito-cc@users.noreply.github.com>
2026-07-08 10:52:20 +0530

    Time: 2 ms (92.17%), Space: 47.8 MB (56.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,38 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums.length > 1) {
-            int w = 0;
+        // if (nums.length > 1) {
+        //     int w = 0;
 
-            for (int r = 0; r < nums.length; r++) {
-                if(nums[r]!=0){
-                    int temp = nums[w];
-                    nums[w]=nums[r];
-                    nums[r]=temp;
-                    w++;
-                }
+        //     for (int r = 0; r < nums.length; r++) {
+        //         if(nums[r]!=0){
+        //             int temp = nums[w];
+        //             nums[w]=nums[r];
+        //             nums[r]=temp;
+        //             w++;
+        //         }
 
+        //     }
+        // }
+
+
+        int r = 0;
+        int w = 0;
+
+        while(r<nums.length){
+            if(nums[r]!=0){
+                int temp = nums[r];
+                nums[r]=nums[w];
+                nums[w]=temp;
+                w++;
             }
+            
+            r++;
         }
+
+
+
+
+
 
     }
 }
