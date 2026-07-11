01f95a6fff3b8d2bc34c0d341c2e8d245845f451
Abhishek Chaudhary (Arito) <147802600+Arito-cc@users.noreply.github.com>
2026-07-11 10:12:18 +0530

    Time: 0 ms (100%), Space: 43.6 MB (43.24%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,38 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         
-        int k = 0;
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i]!=val){
-            nums[k]=nums[i];
-            k+=1;
+        // int k = 0;
+        // for(int i = 0;i<nums.length;i++){
+        //     if(nums[i]!=val){
+        //     nums[k]=nums[i];
+        //     k+=1;
+        //     }
+        // }
+
+        // return k;
+
+
+
+        int r = 0;
+        int w = 0;
+
+        while(r<nums.length){
+            if(nums[r]!=val){
+                nums[w]=nums[r];
+                w++;
+                r++;
+            }
+            else{
+                r++;
             }
         }
 
-        return k;
+        return w;
+
+
+
+
+
+
     }
 }
