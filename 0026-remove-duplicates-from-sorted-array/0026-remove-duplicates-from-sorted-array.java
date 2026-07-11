377c98e5c9656241b21da7445f8859d9981dc52a
Abhishek Chaudhary (Arito) <147802600+Arito-cc@users.noreply.github.com>
2026-07-11 10:04:28 +0530

    Time: 0 ms (100%), Space: 46.8 MB (43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,18 +1,39 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         
-        if (nums.length ==1){
-            return 1;
-        }
-        int write = 0; 
+        // if (nums.length ==1){
+        //     return 1;
+        // }
+        // int write = 0; 
 
-        for(int read = 1; read<nums.length ; read++){
-            if(nums[read]!=nums[write]){
-                write++;
-                nums[write]=nums[read];
+        // for(int read = 1; read<nums.length ; read++){
+        //     if(nums[read]!=nums[write]){
+        //         write++;
+        //         nums[write]=nums[read];
+        //     }
+        // }
+        // return write+1;
+
+
+
+        if(nums.length == 1){
+            return 1;
+        } 
+
+        int r = 1;
+        int w = 1;
+
+        while(r<nums.length){
+            if(nums[r]!=nums[r-1]){
+                nums[w]=nums[r];
+                w++;
+                r++;
+            }
+            else{
+                r++;
             }
         }
-        return write+1;
 
+        return w;
     }
 }
