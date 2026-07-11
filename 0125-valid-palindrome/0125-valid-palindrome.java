b91ec6feabdd06fcde8206f9be169020ce7cf0fc
Abhishek Chaudhary (Arito) <147802600+Arito-cc@users.noreply.github.com>
2026-07-11 10:25:21 +0530

    Time: 3 ms (60.42%), Space: 44.5 MB (57.15%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,58 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
 
+
+
+
+
+    public boolean isPalindrome(String s) {
+        // s = s.toLowerCase();
+
+        // int l = 0;
+        // int r = s.length() - 1;
+        // while (l < r) {
+        //     if (Character.isLetterOrDigit(s.charAt(l))) {
+        //         if (Character.isLetterOrDigit(s.charAt(r))) {
+        //             if (s.charAt(l) != s.charAt(r)) {
+        //                 return false;
+        //             }
+        //             l++;
+        //             r--;
+        //         } else {
+        //             r--;
+        //         }
+        //     } else {
+        //         l++;
+        //     }
+
+        // }
+        // return true;
+
+        // Revision Solving 
+
+        
+        s = s.toLowerCase();
         int l = 0;
-        int r = s.length() - 1;
-        while (l < r) {
-            if (Character.isLetterOrDigit(s.charAt(l))) {
-                if (Character.isLetterOrDigit(s.charAt(r))) {
-                    if (s.charAt(l) != s.charAt(r)) {
+        int r = s.length()-1;
+
+        while(l<r){
+            if(Character.isLetterOrDigit(s.charAt(l))){
+                if(Character.isLetterOrDigit(s.charAt(r))){
+                    if((s.charAt(l)!=(s.charAt(r)))){
                         return false;
                     }
                     l++;
                     r--;
-                } else {
+                }else{
                     r--;
                 }
-            } else {
+            }
+            else{
                 l++;
             }
-
         }
+
         return true;
+
+
     }
 }
