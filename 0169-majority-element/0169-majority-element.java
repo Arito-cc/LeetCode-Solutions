427121b696800fb7aaa42c9cfee6e537fa37d10c
Abhishek Chaudhary (Arito) <147802600+Arito-cc@users.noreply.github.com>
2026-07-14 12:11:21 +0530

    Time: 2 ms (70.16%), Space: 55.7 MB (31.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,27 +1,44 @@
 class Solution {
     public int majorityElement(int[] nums) {
 
-        HashMap <Integer,Integer> frequency = new HashMap<>();
+        // HashMap<Integer, Integer> frequency = new HashMap<>();
 
-        for(int i : nums){
-            if(frequency.containsKey(i)){
-                frequency.put(i,(frequency.get(i))+1);
-            }else{
-                frequency.put(i,1);
-            }
+        // for (int i : nums) {
+        //     if (frequency.containsKey(i)) {
+        //         frequency.put(i, (frequency.get(i)) + 1);
+        //     } else {
+        //         frequency.put(i, 1);
+        //     }
 
-            if(frequency.get(i)>(nums.length/2)){
-                return i;
-            }
-        }
+        //     if (frequency.get(i) > (nums.length / 2)) {
+        //         return i;
+        //     }
+        // }
 
-
-        return -1;
-
+        // return -1;
 
         // Boyer-Moore Algorithm 
 
+        int num = nums[0];
+        int count = 0;
 
-        
+        for (int i : nums) {
+            if (count == 0) {
+                if (i != num) {
+                    num = i;
+                }
+                count++;
+            }else{
+                if(i!=num){
+                    count--;
+                }
+                else{
+                    count++;
+                }
+            }
+        }
+
+        return num;
+
     }
 }
