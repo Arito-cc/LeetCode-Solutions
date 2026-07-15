7dfffe8af25a5786e025bd1e7e275f2680210835
Abhishek Chaudhary (Arito) <147802600+Arito-cc@users.noreply.github.com>
2026-07-15 10:35:27 +0530

    Time: 5 ms (61.44%), Space: 44.2 MB (89.81%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,27 +1,49 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
+        // My Solution 
+
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+
+        // int[] ch1 = new int[26];
+        // int[] ch2 = new int[26];
+
+        // for(int i = 0; i<s.length();i++){
+
+        //     int x = (int) s.charAt(i) - (int) 'a';
+        //     int y = (int) t.charAt(i) - (int) 'a';
+
+        //     ch1[x] = ch1[x]+1;
+        //     ch2[y] = ch2[y]+1;
+        // }
+
+        // if(Arrays.equals(ch1,ch2)){
+        //     return true;
+        // }
+        // return false;
+
+        // Better Solution 
+
         if(s.length() != t.length()){
             return false;
         }
 
-        int[] ch1 = new int[26];
-        int[] ch2 = new int[26];
+        int[] ch = new int[26];
 
         for(int i = 0; i<s.length();i++){
-
-            int x = (int) s.charAt(i) - (int) 'a';
-            int y = (int) t.charAt(i) - (int) 'a';
-
-            ch1[x] = ch1[x]+1;
-            ch2[y] = ch2[y]+1;
+            ch[(int)s.charAt(i)-(int)'a']++;
+            ch[(int)t.charAt(i)-(int)'a']--;
         }
 
-        if(Arrays.equals(ch1,ch2)){
-            return true;
+        for(int x : ch){
+            if(x!=0){
+                return false;
+            }
         }
-        return false;
 
+        return true;
 
 
     }
